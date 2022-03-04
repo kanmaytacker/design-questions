319531fdcd547c2ea67b896430d085e0b689db52
Tanmay <tanmaykacker40@gmail.com>
2022-03-04 14:32:01 +0000

    Uses game repository.

--- src/main/java/com/scaler/lld/snakesladders/repositories/GameRepository.java
+++ src/main/java/com/scaler/lld/snakesladders/repositories/GameRepository.java
@@ -6,11 +6,20 @@ import java.util.List;
 import com.scaler.lld.snakesladders.models.Game;
 
 public class GameRepository {
-    
+
     List<Game> games = new ArrayList<>();
 
     public Game save(Game game) {
         this.games.add(game);
         return game;
+    }
+
+    public Game findById(Integer gameId) {
+        for (Game game : games) {
+            if (game.getId().equals(gameId)) {
+                return game;
+            }
+        }
+        return null;
     }
 }
