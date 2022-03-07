64f0213550a431b71d2671db1432812487f6d502
Tanmay <tanmaykacker40@gmail.com>
2022-03-07 14:22:27 +0000

    Adds implementation of entry gate flow.

--- src/main/java/com/scaler/lld/parkinglot/controllers/EntryGateController.java
+++ src/main/java/com/scaler/lld/parkinglot/controllers/EntryGateController.java
@@ -1,5 +1,13 @@
 package com.scaler.lld.parkinglot.controllers;
 
+import com.scaler.lld.parkinglot.dtos.GetTicketDTO;
+import com.scaler.lld.parkinglot.models.VehicleType;
+import com.scaler.lld.parkinglot.services.EntryGateService;
+
 public class EntryGateController {
-    
+    EntryGateService entryGateService = new EntryGateService();
+
+    public GetTicketDTO createTicket(VehicleType vehicleType) {
+        return entryGateService.createTicket(vehicleType);
+    }
 }
--- src/main/java/com/scaler/lld/parkinglot/services/EntryGateService.java
+++ src/main/java/com/scaler/lld/parkinglot/services/EntryGateService.java
@@ -1,11 +1,36 @@
 package com.scaler.lld.parkinglot.services;
 
 import com.scaler.lld.parkinglot.dtos.GetTicketDTO;
+import com.scaler.lld.parkinglot.models.ParkingSpot;
+import com.scaler.lld.parkinglot.models.SpotStatus;
+import com.scaler.lld.parkinglot.models.Ticket;
 import com.scaler.lld.parkinglot.models.VehicleType;
 
 public class EntryGateService {
-    
+
+    ParkingSpotService parkingSpotService = new ParkingSpotService();
+    SlotAllocationService allocationService = new SlotAllocationService();
+    TicketService ticketService = new TicketService();
+
     public GetTicketDTO createTicket(VehicleType vehicleType) {
-        return null;
+
+        // Early returns
+        ParkingSpot parkingSpot = allocationService.allocateSlot(vehicleType);
+        if (parkingSpot == null) {
+            throw new RuntimeException("Slot not available!");
+        }
+
+        // Update parking spot
+        parkingSpot.setSpotStatus(SpotStatus.FILLED);
+        parkingSpotService.markSlotBooked(parkingSpot);
+
+        // Create and persist ticket
+        Ticket ticket = ticketService.createTicket(vehicleType, parkingSpot);
+
+        return GetTicketDTO
+                .builder()
+                .entryTime(ticket.getEntryTime())
+                .vehicleType(ticket.getVehicleType())
+                .build();
     }
 }
