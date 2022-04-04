360a6b89311ac1d7c3126ed53a661baa0ff38956
Tanmay <tanmaykacker40@gmail.com>
2022-04-04 15:47:32 +0100

    Removes strategies.

--- src/main/java/com/scaler/lld/parkinglot/services/FeeCalculatorService.java
+++ src/main/java/com/scaler/lld/parkinglot/services/FeeCalculatorService.java
@@ -1,13 +1,5 @@
 package com.scaler.lld.parkinglot.services;
 
-import com.scaler.lld.parkinglot.models.Ticket;
-import com.scaler.lld.parkinglot.strategies.FeesStrategy;
-import com.scaler.lld.parkinglot.strategies.timebased.TimeBasedFeesStrategyFactory;
-
 public class FeeCalculatorService {
     
-    public int calculateFees(Ticket ticket) {
-        FeesStrategy feesStrategy = TimeBasedFeesStrategyFactory.getStrategy(ticket.getVehicleType());
-        return feesStrategy.calculateFees(ticket);
-    }
 }
--- src/main/java/com/scaler/lld/parkinglot/strategies/FeesStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.scaler.lld.parkinglot.strategies;
-
-import com.scaler.lld.parkinglot.models.Ticket;
-
-public interface FeesStrategy {
-    int calculateFees(Ticket ticket);
-}
