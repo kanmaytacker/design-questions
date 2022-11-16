2a0891385d782d52f9e8afb6f21e7716a71bb68e
Tanmay <tanmaykacker40@gmail.com>
2022-11-16 15:02:19 +0000

    Adds base application.

---
+++ chess/src/main/java/com/scaler/lld/chess/ChessApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.lld.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessApplication.class, args);
+    }
+
+}
---
+++ chess/src/test/java/com/scaler/lld/chess/ChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.lld.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
