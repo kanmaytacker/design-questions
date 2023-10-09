04ff2375d660b14a17a907ddec418efaf29ecb0a
Tanmay <tanmaykacker40@gmail.com>
2023-10-09 16:15:47 +0100

    Updates compile error.

--- src/main/java/com/scaler/lld/kata/models/Board.java
+++ src/main/java/com/scaler/lld/kata/models/Board.java
@@ -3,8 +3,8 @@ package com.scaler.lld.kata.models;
 import lombok.Getter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Getter
 public class Board {
@@ -18,29 +18,21 @@ public class Board {
     }
 
     private List<List<BoardCell>> initialiseCells(int size) {
-        // From int 3 -> [], [], []
-        //               [], [], []
-        //               [], [], []
-
-        // Create the first row
-        List<BoardCell> firstRow = Collections.nCopies(size, new BoardCell());
-        return Collections.nCopies(size, firstRow);
+        List<List<BoardCell>> cells = new ArrayList<>();
+        IntStream.range(0, size).forEach(row -> {
+            List<BoardCell> rowCells = new ArrayList<>();
+            IntStream.range(0, size).forEach(column -> rowCells.add(new BoardCell(row, column)));
+            cells.add(rowCells);
+        });
+        return cells;
     }
 
     public boolean isEmpty(int row, int column) {
-        new Bo
         return getBoardCell(row, column).getSymbol() == null;
     }
 
     public void update(BoardCell move) {
-
-        int row = move.getRow();
-        int column = move.getColumn();
-
-        BoardCell cell = getBoardCell(row, column);
-        cell.setRow(row);
-        cell.setColumn(column);
-        cell.setSymbol(move.getSymbol());
+        getBoardCell(move.getRow(), move.getColumn()).setSymbol(move.getSymbol());
     }
 
     private BoardCell getBoardCell(int row, int column) {
--- src/main/java/com/scaler/lld/kata/models/BoardCell.java
+++ src/main/java/com/scaler/lld/kata/models/BoardCell.java
@@ -4,10 +4,16 @@ import lombok.*;
 
 
 
-@NoArgsConstructor
-@Builder
+@AllArgsConstructor
+@Getter
+@Setter
 public class BoardCell {
     private int row;
     private int column;
     private GameSymbol symbol;
+
+    public BoardCell(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 }
