4c43f83c55f6d760f1454f505a349c92c8c6fe94
Arti <artiprajapati3223@gmail.com>
2023-10-10 22:10:57 +0530

    Testing branch

--- src/main/java/com/scaler/lld/tictactoe/models/GameSymbol.java
+++
@@ -1,5 +0,0 @@
-package com.scaler.lld.tictactoe.models;
-
-public enum GameSymbol {
-    X, O
-}
--- src/main/java/com/scaler/lld/tictactoe/strategies/PlayingStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.scaler.lld.tictactoe.strategies;
-
-import com.scaler.lld.tictactoe.models.Board;
-import com.scaler.lld.tictactoe.models.BoardCell;
-
-public interface PlayingStrategy {
-    BoardCell play(Board board);
-}
